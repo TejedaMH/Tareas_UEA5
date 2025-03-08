29c460444b62071309050cace0b40cc6bd15ae51
TejedaMH <166840521+TejedaMH@users.noreply.github.com>
2025-03-08 06:17:14 +0000

    Add files via upload

---
+++ Tarea 2_Respuesta_critica/script.js
@@ -0,0 +1,9 @@
+let p1 = "Mi acercamiento al código y a los diversos lenguajes de programación, como HTML, CSS y JavaScript, durante la carrera universitaria me han permitido descubrir posibilidades que nunca había considerado. Al principio, veía la programación solo como una herramienta para la creación de arte generativo, pero comprendí que su potencial va más allá de eso. Es posible utilizar recursos multimedia, como la cámara y/o la ubicación de la computadora, para generar interactividad y desarrollar piezas únicas.";
+
+let p2 = "Esta interactividad nos permite que cada pieza responda a factores en tiempo real, creando así experiencias personalizadas y dinámicas. Al integrar elementos multimedia como audio, video o datos en vivo, la programación se convierte en una herramienta versátil para la expresión y narrativa artística. Explorar estas posibilidades me ha llevado a replantear los límites que hay en el arte y la tecnología, descubriendo la importancia de la interacción del usuario para transformar y enriquecer obras.";
+
+document.getElementById('parrafo1').innerHTML = p1;
+document.getElementById('parrafo2').innerHTML = p2;
+
+console.log(p1.length);
+console.log(p2.length);  
