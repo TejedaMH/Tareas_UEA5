a230d0522eeae2ee22b1b2a7ccd835f910805add
TejedaMH <166840521+TejedaMH@users.noreply.github.com>
2025-04-05 05:49:27 +0000

    Add files via upload
    Tarea multimedia

--- Tarea 3_Multimedia_web/script.js
+++ Tarea 3_Multimedia_web/script.js
@@ -1,16 +1,13 @@
 console.log ("RGB: Simulación del color")
 
-
-//PEQUEÑOS DETALLES DE SUMA SOBERBÍA
-
-// Botón y audio
 const playButton = document.getElementById('playButton');
 const audio = document.getElementById('audio');
 
-// Función para controlar el audio
 playButton.addEventListener('click', function() {
            
-// Si el audio está pausado, se reproduce y viceversa
+
+
+    
 if (audio.paused) {
     audio.play();
     playButton.textContent = 'Pausar Audio'; // Cambia el texto del botón si se reproduce el audio
